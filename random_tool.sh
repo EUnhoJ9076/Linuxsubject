78b317cc55a639c33adc02b3ad5585b96a62b5ae
Eunho <jeh7854@gmail.com>
2025-11-30 19:40:44 +0900

    Add feature/lotto

--- random_tool.sh
+++ random_tool.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-roll_dice() {
-    echo "주사위: $((RANDOM % 6 + 1))"
+lotto() {
+    echo "로또 번호(1~45):"
+    for i in {1..6}; do
+        echo -n "$((RANDOM % 45 + 1))"
+    done
+    echo 
 }
